f88b403f37d75d0d333e448a2e1a7236f40b56d1
kylemurdoch <kylemurdoch@shaw.ca>
2020-02-17 14:07:14 -0800

    first commit

---
+++ precache-manifest.531249fb8b629983264aa0f20855bd1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c365d527faae306b84f12cdbab2aaa14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426ffed4143dedf37fcb",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "88e17cc9e692fc5c2204",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "426ffed4143dedf37fcb",
+    "url": "/static/js/2.85b310b9.chunk.js"
+  },
+  {
+    "revision": "1bcca0652757041a450130f0152487c8",
+    "url": "/static/js/2.85b310b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e17cc9e692fc5c2204",
+    "url": "/static/js/main.910228b7.chunk.js"
+  },
+  {
+    "revision": "3426f55a292a42a74676",
+    "url": "/static/js/runtime-main.df981bb7.js"
+  }
+]);
